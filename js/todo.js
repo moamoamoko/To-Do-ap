b89236dfd0e408117734fb20a9b9971fc1a100ed
Reo Sugawara <sugawara@professy.co.jp>
2023-09-01 21:27:50 +0900

    add 9/1

---
+++ js/todo.js
@@ -0,0 +1,10 @@
+// ここにjavascriptを記載
+
+// ダイアログテスト
+const confirmDialog = () => {
+  window.confirm("確認ダイアログのテスト");
+}
+
+const fwjMovement = () => {
+  window.open('https://fwj.jp/', '_blank');
+}
