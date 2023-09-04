a234c5d9c8930dad52af33de051df08d07cd5098
Reo Sugawara <sugawara@professy.co.jp>
2023-09-04 18:25:31 +0900

    add task add button

--- js/todo.js
+++ js/todo.js
@@ -15,3 +15,14 @@ const fwjconfirmDialog = () => {
 const fwjMovement = () => {
   window.open('https://fwj.jp/', '_blank');
 }
+
+document.querySelector('.add_task_button').addEventListener('click', () => {
+  const newForm = document.createElement('input');
+  newForm.type = 'text';
+
+  const newLabel = document.createElement('label');
+  newLabel.textContent = 'タスク名';
+
+  newLabel.appendChild(newForm);
+  document.querySelector('.add_task').appendChild(newLabel);
+});
